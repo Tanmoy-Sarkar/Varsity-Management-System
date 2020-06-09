7b6d0197e485c8d2b0d35d82cb4010f8ff2ab78e
Tanmoy Sarkar <tanmoy9920@gmail.com>
2020-06-09 17:35:16 +0600

    improving schedule functionality

--- schedule/models.py
+++ schedule/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import When
 from django.urls import reverse
 # Create your models here.
 class Schedule(models.Model):
@@ -7,14 +8,42 @@ class Schedule(models.Model):
 	('B','B'),
 	('C','C'),
 	('D','D'),
-	('F','F'),
+	('E','E'),
 	]
 	day = models.CharField(max_length=1,choices=DAY_CHOICES)
 	date = models.DateField(auto_now_add=True)
 
-
 	def __str__(self):
 		return self.day
 
-	
+class Routine(models.Model):
+	DAY_CHOICES = [
+	('A','A'),
+	('B','B'),
+	('C','C'),
+	('D','D'),
+	('E','E'),
+	]
+	day = models.CharField(max_length=1,choices=DAY_CHOICES)
+
+	morning_lab = models.BooleanField(default = False)
+	first_period = models.CharField(max_length=30, null = " ",blank=True,default = "-")
+	second_period = models.CharField(max_length=30, null = " ",blank=True,default = "-")
+	third_period = models.CharField(max_length=30, null = " ",blank=True,default = "-")
+
+	after_morning_lab=models.BooleanField(default = False)	
+	fourth_period = models.CharField(max_length=30, null = " ",blank=True,default = "-")
+	fifth_period = models.CharField(max_length=30, null = " ",blank=True,default = "-")
+	sixth_period = models.CharField(max_length=30, null = " ",blank=True,default = "-")
+
+	afternoon_lab=models.BooleanField(default = False)
+	seventh_period = models.CharField(max_length=30, null = " ",blank=True,default = "-")
+	eigth_period = models.CharField(max_length=30, null = " ",blank=True,default = "-")
+	ninth_period = models.CharField(max_length=30, null = " ",blank=True,default = "-")
+
+	def __str__(self):
+		return self.day + " Day Routine"
+
+
+
 	
--- schedule/views.py
+++ schedule/views.py
@@ -1,13 +1,29 @@
 from django.shortcuts import render
-from .models import Schedule
+from .models import Schedule,Routine
 # Create your views here.
 
 def ScheduleView(request):
 	day = Schedule.objects.all()[0]
-	routineB = "first period is programming"
-	context ={
-	'day':day,
-	'routine':routineB,
+	context = {
+		"day":day,
 	}
 
+	#finding which day is present day and showing the routine according to that
+	if str(day) == "A":
+		routine=Routine.objects.get(day="A")
+	elif str(day) == "B":
+		routine=Routine.objects.get(day="B")
+	elif str(day) == "C":
+		routine=Routine.objects.get(day="C")
+	elif str(day) == "D":
+		routine=Routine.objects.get(day="D")
+	elif str(day) == "E":
+		routine=Routine.objects.get(day="E")
+	context["routine"] = routine
+
+	#finding all the remaining day schedule excluding the present day
+	remaining_routine = Routine.objects.exclude(day=day)
+	context["remaining_routine"] = remaining_routine
+
 	return render(request,'schedule.html',context)
+
