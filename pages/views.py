16456e7d31c5e10c8697c524f62468e1cb1a1dbe
Tanmoy Sarkar <tanmoy9920@gmail.com>
2020-06-11 12:42:26 +0600

    improving home page views

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from schedule.models import Routine,Schedule
 from schedule.views import get_routine
+from academicnotice.models import AcademicNotice
+from ClassNotice.models import ClassNotice
 # Create your views here.
 
 def HomePageView(request):
@@ -10,6 +12,12 @@ def HomePageView(request):
 	}
 	routine = get_routine(str(day))
 	context["routine"] = routine
+
+	academic_notices = AcademicNotice.objects.all()
+	context["academic_notices"] = academic_notices
+
+	class_notices = ClassNotice.objects.all()
+	context["class_notices"] = class_notices
 
 	return render(request,'home.html',context)
 
